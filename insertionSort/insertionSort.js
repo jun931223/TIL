840ac0f91f718bc47288e96fd274cccdad0c9c7e
이준희 <jun931223@gmail.com>
2021-03-01 13:37:27 +0900

    insertSort

---
+++ insertionSort/insertionSort.js
@@ -0,0 +1,20 @@
+const insertionSort = function (arr) {
+    let sorted = [arr[0]];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] >= sorted[i - 1]) {
+        sorted.push(arr[i]);
+      } else {
+        for (let j = 0; j < i; j++) {
+          if (arr[i] <= sorted[j]) {
+            const left = sorted.slice(0, j);
+            const right = sorted.slice(j);
+            sorted = left.concat(arr[i], right);
+            break;
+          }
+        }
+      }
+    }
+  
+    return sorted;
+  };
+  
