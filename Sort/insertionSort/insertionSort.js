4a11a86c40a547450a794cf01d715c205aaaa5e3
이준희 <jun931223@gmail.com>
2021-03-09 17:52:41 +0900

    sorted arr

--- Sort/insertionSort/insertionSort.js
+++ Sort/insertionSort/insertionSort.js
@@ -1,20 +1,21 @@
-const insertionSort = function (arr) {
-    let sorted = [arr[0]];
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] >= sorted[i - 1]) {
-        sorted.push(arr[i]);
-      } else {
-        for (let j = 0; j < i; j++) {
-          if (arr[i] <= sorted[j]) {
-            const left = sorted.slice(0, j);
-            const right = sorted.slice(j);
-            sorted = left.concat(arr[i], right);
-            break;
-          }
-        }
+let insertionSort = (arr, callMe=(ele)=>ele) =>{ 
+  let sortedArr = [arr[0]]
+   for(let i =1 ; i< arr.length; i++){
+    if(callMe(arr[i]) >= callMe(sortedArr[i-1])){
+    sortedArr.push(arr[i])
+    // 선택된 요소가 비교대상보다 큰수일 경우
+   }else {
+    // 선택된 요소가 비교대상보다 작은수일 경우
+    for(let j=0; j <i; j++){
+   if(callMe(arr[i]) <= callMe(sortedArr[j])){
+   let smaller = sortedArr.slice(0,j)
+   let bigger = sortedArr.slice(j)
+   sortedArr = smaller.concat(arr[i],bigger)
+    break
       }
     }
+  }
   
-    return sorted;
-  };
-  
+ } 
+return sortedArr
+}
