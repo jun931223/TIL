9623a3cb21080e976013c6e2b876707fdb8adb38
Tate <jun931223@gmail.com>
2021-03-12 23:05:07 +0900

    edited

--- programer/one.js
+++ programer/one.js
@@ -1,14 +1,14 @@
 function solution(numbers) {
-  var answer = [];
+  var res = [];
   for (let i = 0; i < numbers.length; i++) {
     for (let j = 0; j < numbers.length; j++) {
       if (i !== j) {
         let addNum = numbers[i] + numbers[j];
-        if (answer.indexOf(addNum) === -1) answer.push(addNum);
+        if (res.indexOf(addNum) === -1) res.push(addNum);
       }
     }
   }
-  return answer.sort(function (a, b) {
+  return res.sort(function (a, b) {
     return a - b;
   });
 }
