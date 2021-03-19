fad03019143850501340bb564cba4500393266da
이준희 <jun931223@gmail.com>
2021-03-19 22:15:05 +0900

    leetcode

--- programer/reversNum.js
+++ programer/reversNum.js
@@ -1,24 +1,27 @@
 var reverse = function (x) {
+  let maxOrMin = Math.pow(2, 31);
   let cnt;
   if (x < 0) {
     cnt = 0;
   } else {
     cnt = -1;
   }
+
   let strNum = x + "";
   let res = "";
+
   for (let i = strNum.length - 1; i > cnt; i--) {
     res = res + strNum[i];
   }
+
   if (x < 0) {
     let putMinus = "-" + res;
-    if (
-      Number(putMinus) < -Math.pow(2, 31) ||
-      Number(res) > Math.pow(2, 31) - 1
-    ) {
+    if (Number(putMinus) < -maxOrMin) {
       return 0;
     }
     return Number(putMinus);
+  } else if (Number(res) > maxOrMin - 1) {
+    return 0;
   } else {
     return Number(res);
   }
